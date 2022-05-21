22b03e9ddf01c121c604e8d43aea82fcfec60020
njm-5558 <najmebabai@gmail.com>
2022-05-22 02:57:15 +0430

    making a weather app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Weather from "./Weather"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Weather defaultcity="New York"/>
+      <footer className="GitHub-link">
+  
+          <a href="https://github.com/njm-5558/wearhet-react">
+            Open-source code
+          </a>
+          <span> ,by Najme Babai, SheCodes student</span>
+      </footer>
     </div>
   );
 }
