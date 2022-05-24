66cbd5da6c12f9d97f4bf16e82cfe78e1749f9e7
njm-5558 <najmebabai@gmail.com>
2022-05-24 22:20:55 +0430

    adding new component to change unites

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,7 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherTemperature from "./WeatherTemperature";
-import WeatherTemperature from "./WeatherTemperature";
+
 
 export default function WeatherInfo(props){ 
 return (
